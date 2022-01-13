9fea16c235cdc04ae35d08104810e8224293d10f
karthi2410 <88647141+karthi2410@users.noreply.github.com>
2022-01-13 22:33:38 +0530

    Update SidebarOption.js

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./SidebarOption.css";
-import app from "./app";
+
 function SidebarOption({ option = "test", Icon }) {
   return (
     <div className="sidebarOption">
